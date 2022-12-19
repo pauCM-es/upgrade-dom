828040d01d135d3cf38245ea03ef7d7557efc461
pauCM-es <costamingarro@gmail.com>
2022-12-19 19:29:29 +0100

    iteration 1 - hasta 1.4

---
+++ iteration1.js
@@ -0,0 +1,23 @@
+//? 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+const showMe$$ = document.querySelector(".showme")
+console.log(showMe$$)
+
+//? 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const h1$$ = document.querySelector("#pillado")
+console.log(h1$$)
+
+//? 1.3 Usa querySelector para mostrar por consola todos los p
+const p$$ = document.querySelectorAll("p")
+console.log(p$$)
+
+//? 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+const pokemon$$ = document.querySelector(".pokemon")
+console.log(pokemon$$)
+
+//? 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+//? data-function="testMe"
+
+
+//? 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+//? data-function="testMe"
+
