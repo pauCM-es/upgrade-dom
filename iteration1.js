0de7551f92cb2b84626a99a937e9f1d1773c2526
pauCM-es <costamingarro@gmail.com>
2022-12-20 09:46:38 +0100

    fix ida de olla con data-function

--- iteration1.js
+++ iteration1.js
@@ -16,24 +16,9 @@ console.log(pokemon$$)
 
 //? 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
 //? data-function="testMe"
-//* opcion 1. Usando los <span> como selector
-// const elements = Array.from(document.querySelectorAll("span"))
-// console.log(elements[0].attributes["data-function"].value === "testMe" ? "funciona" : "mierder")
 
-//* opcion 2. Si no supieramos que elementos tienen ese attributo
-
-const elements = Array.from(document.body.children)
-const withAttribute = []
-
-elements.forEach(element => {
-  // element.attributes["data-function"].value === "testMe" ? "funciona" : "mierder"
-  if (element.hasAttribute("data-function") && element.attributes["data-function"].value === "testMe") {
-    withAttribute.push(element)
-  }
-})
-
-console.log(withAttribute)
+const dataFunctions$$ = document.querySelector("[data-funtion='testMe']")
 
 //? 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
 //? data-function="testMe"
-console.log(withAttribute[2].innerText)
+console.log(Array.from.dataFunctions$$[2].innerText)
